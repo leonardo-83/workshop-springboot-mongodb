89c8d0a313b1fe19041d0845d74a3245bc60087f
Leonardo <leonardocarpino@gmail.com>
2021-07-13 11:57:55 -0300

    Consulta simples com query methods

--- src/main/java/com/almeidaleonardo/workshopmongo/services/PostService.java
+++ src/main/java/com/almeidaleonardo/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.almeidaleonardo.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,8 @@ public class PostService {
 		}else {
 			throw new ObjectNotFoundException("Objeto nao encontrado");
 		}
+	}
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
